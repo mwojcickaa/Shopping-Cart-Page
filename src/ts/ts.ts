7de443ee9f299d5752d703293bb66e2892ee90b1
Martyna <martynawojcickaaa@gmail.com>
2023-09-01 21:50:16 +0200

    Fix input

--- src/ts/ts.ts
+++ src/ts/ts.ts
@@ -12,6 +12,9 @@ for (let i = 0; i < 3; i++) {
             buttonDecrement.disabled = false;
         } else if (valueInput.value == "4") {
             buttonIncrement.disabled = true;
+        } else if (valueInput.value == "") {
+            valueInput.value = "0";
+            valueInput.classList.remove("empty-input");
         }
         valueInput.value = String(parseInt(valueInput.value) + 1);
     });
@@ -25,29 +28,39 @@ for (let i = 0; i < 3; i++) {
         valueInput.value = String(parseInt(valueInput.value) - 1);
     })
 
+    valueInput.addEventListener('keypress', detectSigns);
+
+    function detectSigns(e: KeyboardEvent) {
+        if (e.key == "e" || e.key == "," || e.key == "." || e.key == "-" || e.key == "+") {
+            e.preventDefault();
+        };
+    };
+
     valueInput.addEventListener("input", () => {
         let convertedNumberInput = Number(valueInput.value)
 
         if (valueInput.value == "") {
-            valueInput.style.backgroundColor = "rgba(255, 0, 0, 0.1)"
-            valueInput.style.borderRadius = "3px"
+            valueInput.classList.add("empty-input");
+            buttonDecrement.disabled = true;
+            buttonIncrement.disabled = false;
             return;
-        } else if (convertedNumberInput < 1) {
+        } else if (convertedNumberInput <= 1) {
+            valueInput.classList.remove("empty-input");
             valueInput.value = "1"
             buttonIncrement.disabled = false;
             buttonDecrement.disabled = true;
-            valueInput.style.backgroundColor = "#F0F0F0"
         } else if (convertedNumberInput >= 5) {
-            valueInput.value = "5"
+            valueInput.classList.remove("empty-input");
+            valueInput.value = "5";
             buttonIncrement.disabled = true;
             buttonDecrement.disabled = false;
-            valueInput.style.backgroundColor = "#F0F0F0"
-        } else if (convertedNumberInput > 1 && convertedNumberInput < 5) {
+        } else if (convertedNumberInput >= 1 && convertedNumberInput <= 5) {
+            valueInput.classList.remove("empty-input");
+            valueInput.classList.add("full-input");
             buttonIncrement.disabled = false;
             buttonDecrement.disabled = false;
-            valueInput.style.backgroundColor = "#F0F0F0"
-        }
-    })
+        };
+    });
 };
 
 
